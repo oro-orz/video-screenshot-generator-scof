b98a8532e2e5fdc1e4327584da0c102d1d2d1a3e
oro-orz <oyama@timingood.co.jp>
2024-10-16 10:01:42 +0900

    v2

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import VideoScreenshotGenerator from '../components/VideoScreenshotGenerator'
+
+export default function Home() {
+  return (
+    <div>
+      <VideoScreenshotGenerator />
+    </div>
+  )
+}
